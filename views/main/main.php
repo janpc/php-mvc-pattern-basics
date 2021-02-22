165fdac8884e76288b755680aada89a04b830602
Jan Puig <janpuigcapella@gmail.com>
2021-02-22 09:38:05 +0100

    main view

--- views/main/main.php
+++ views/main/main.php
@@ -1,9 +1,23 @@
-<!-- This is the main generic view of your application, it's not required to use it -->
-<?php
-    echo 'main<br>';
-    echo "<a href='index.php'>index</a><br>";
-    echo "<a href='index.php?controller=song'>songs</a><br>";
-    echo "<a href='index.php?controller=artist'>artists</a><br>";
-    echo "<a href='index.php?controller=song&id=1'>songs 1</a><br>";
-    echo "<a href='index.php?controller=artist&id=1'>artists 1</a><br>";
-?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/main.css">
+    
+</head>
+<body>
+    <?php
+        echo "
+            <a class='main__songs' href='index.php?controller=song'>Songs</a>
+            <a class='main__artists' href='index.php?controller=artist'>Artists</a>
+        ";
+    ?>
+    </section>
+</body>
+</html>
