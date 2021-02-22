0704e59077242ee05ae140e558d049790a432d75
Jan Puig <janpuigcapella@gmail.com>
2021-02-22 15:48:00 +0100

    artist view

--- views/artist/artist.php
+++ views/artist/artist.php
@@ -1,6 +1,47 @@
 <?php
-print_r($artist['artist_name'] . '<br><img src="' . $artist['picture'] . '"/><br>' . $artist['info'] . '<br>' . $artist['artist_id'] . '<br>');
 
-foreach ($artist['songs'] as $song) {
-    print_r($song['song_name'] . '<br><img src="' . $song['cover'] . '"/><br>');
-}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+        echo '<title>' .$artist['artist_name'] . '</title>';
+    ?>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/artist.css">
+    
+</head>
+<body>
+    <?php
+        include './assets/html/header.html';
+
+        echo "<section class='artist' >
+                <div class='artist__img' style='background-image: url(" . $artist['picture'] . ");'>
+                </div>
+                <div class='artist__info'>
+                    <h1 class='artist__info__name'>" .$artist['artist_name'] . "</h1>
+                    <p class='artist__info__bio'>" . $artist['info'] . "</p>
+                </div>
+            </section>
+            <div class='artist__songs__container'>";
+        foreach ($artist['songs'] as $song) {
+            echo "<a class='artist__song' href='index.php?controller=song&id=" . $song['song_id'] . "'>
+                    <div class='artist__song__img' style='background-image: url(" . $song['cover'] . ");'>
+                    </div>
+                    <h2 class='artist__song__name'>" . $song['song_name'] . "</h2>
+                </a>";
+        }
+
+        echo "</div>";
+
+
+        
+    ?>
+</body>
+</html>
